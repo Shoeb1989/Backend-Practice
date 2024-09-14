701d0870f35d87b87fb8ac1a82a3ad6133a17360
Shoeb1989 <shoebsikder1@gmail.com>
2024-09-14 21:52:35 +0600

    auth failed

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,15 +1,27 @@
 
-import {getAuth} from 'firebase/auth';
+import {getAuth, signInWithPopup} from 'firebase/auth';
 import app from '../../firebase/firebase.init';
+import { GoogleAuthProvider } from 'firebase/auth/web-extension';
 
 const Login = () => {
 
     const auth = getAuth(app);
     const provider = new GoogleAuthProvider();
+    const handleGoogleSignIN = () => {
+        signInWithPopup(auth, provider)
+        .then(result => {
+            const user = result.user;
+            console.log(user);
+        })
+            .catch(error => {
+                console.log('error', error.message)
+            })
+        
+    }
     return (
         <div>
 
-            <button>Google Login</button>
+            <button onClick={handleGoogleSignIN} >Google Login</button>
             
         </div>
     );
