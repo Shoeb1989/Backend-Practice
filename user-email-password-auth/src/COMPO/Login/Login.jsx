46a06a9d77ee9eb03ee2b1fe440b79102b90daba
Shoeb1989 <shoebsikder1@gmail.com>
2024-09-16 00:29:08 +0600

    forget pass auth working

--- user-email-password-auth/src/COMPO/Login/Login.jsx
+++ user-email-password-auth/src/COMPO/Login/Login.jsx
@@ -1,9 +1,96 @@
-import React from 'react';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import React, { useState } from 'react';
+import auth from '../../Firebase/Firebase.config';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+
+
+    const [registererror, setRegistererror] = useState('');
+    const [success, setSuccess] = useState('');
+
+
+
+
+
+    const handleLogin = e => {
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        console.log(email, password);
+
+
+        setRegistererror('');
+        setSuccess('');
+
+
+        // add validation
+        signInWithEmailAndPassword(auth, email, password)
+            .then(result => {
+                console.log(result.user);
+                setSuccess('Login Successfull');
+            })
+            .catch(error => {
+                console.error(error);
+                setRegistererror(error.message);
+            })
+
+
+    }
+
+
+    const handleForgetPass = e => {
+        console.log('send reset pass')
+
+    }
+
+
     return (
-        <div>
-            <h2 className='text-3xl' > Login </h2>
+        <div className="hero bg-base-200 min-h-screen">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <div className="text-center lg:text-left">
+                    <h1 className="text-5xl font-bold">Login now!</h1>
+                    <p className="py-6">
+                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+                        quasi. In deleniti eaque aut repudiandae et a id nisi.
+                    </p>
+                </div>
+                <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                    <form onSubmit={handleLogin}
+                        className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" placeholder="email" name='email' className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" placeholder="password" name='password' className="input input-bordered" required />
+                            <label className="label">
+                                <a onClick={handleForgetPass} href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">Login</button>
+                        </div>
+
+
+
+                    </form>
+                    {
+                        registererror && <p className='text-red-500' >{registererror}</p>
+                    }
+
+                    {
+                        success && <p className='text-green-700' >{success}</p>
+                    }
+
+                    <p>New to this website please <Link className='btn btn-primary' to='/register' >Register</Link> </p>
+                </div>
+            </div>
         </div>
     );
 };
--- user-email-password-auth/src/COMPO/Register/Register.jsx
+++ user-email-password-auth/src/COMPO/Register/Register.jsx
@@ -1,28 +1,46 @@
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import React, { useState } from 'react';
 import auth from '../../Firebase/Firebase.config';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
-      // error shows
+
+    const [Showpass, setShowpass] = useState(false);
+
+    // error shows
     const [registererror, setRegistererror] = useState('');
-    
+
     // succ message
 
     const [success, setSuccess] = useState('')
 
     const handleRegister = e => {
         e.preventDefault();
-        
+
         const email = e.target.email.value;
         const password = e.target.password.value;
-
-        console.log(email, password);
+        const accepted = e.target.terms.checked;
+        console.log(email, password, accepted);
 
         if (password.length < 6) {
             setRegistererror('Need 6 characters');
             return;
 
         }
+
+        else if (!/[A-Z]/.test(password)) {
+            setRegistererror('Need 1 uppercase');
+            return;
+        }
+
+        // checkbox
+
+        else if (!accepted) {
+            setRegistererror('please accept our terms and condition')
+            return;
+        }
+
+
 
         // pass type 6 length
 
@@ -34,16 +52,16 @@ const Register = () => {
 
         // create user
 
-     createUserWithEmailAndPassword(auth, email, password)
-     .then(result =>{
-        console.log(result.user);
-        setSuccess('Create Successfull')
-        
-     })
-     .catch(error => {
-        console.error(error);
-        setRegistererror(error.message);
-     })
+        createUserWithEmailAndPassword(auth, email, password)
+            .then(result => {
+                console.log(result.user);
+                setSuccess('Create Successfull')
+
+            })
+            .catch(error => {
+                console.error(error);
+                setRegistererror(error.message);
+            })
 
     }
 
@@ -51,24 +69,39 @@ const Register = () => {
     return (
         <div className=' border' >
             <div className='mx-auto md:w-1/2 '  >
-            <h2 className="text-3lx mb-8 ">Please Register</h2>
-            <form onSubmit={handleRegister} >
-                <input className='mb-4 w-3/4 py-2 px-4 border-2 ' type="email" name="email" placeholder='Email Address' required  />
-                <br />
-                <br />
-                <input className='mb-4 w-3/4 py-2 px-4 border-2 ' type="password" name="password" placeholder='Password'  />
-                <br />
-                <br />
-                <input className='mb-4 w-3/4 btn btn-secondary ' type="submit" value="Register" />
-            </form>
-            {
-                Error && <p className='text-red-500' >{registererror}</p>
-            }
+                <h2 className="text-3lx mb-8 ">Please Register</h2>
+                <form onSubmit={handleRegister} >
+                    <input className='mb-4 w-3/4 py-2 px-4 border-2 ' type="email" name="email" placeholder='Email Address' required />
+                    <br />
+                    <br />
+                    <input
+                        className='mb-4 w-3/4 py-2 px-4 border-2 '
+                        type={Showpass ? "text" : "password"}
+                        name="password"
+                        placeholder='Password' />
+                    <span onClick={() => setShowpass(!Showpass)} >Show</span>
+                    <br />
+                    <div className='mb-2' >
+                        <input type="checkbox" name="terms" id="terms" />
+                        <label className='ml-2'
+                            htmlFor="terms"> Accept out condition</label>
 
-            {
-                success && <p className='text-green-700' >{success}</p>
-            }
+                    </div>
+                    <br />
+                    <input className='mb-4 w-3/4 btn btn-secondary ' type="submit" value="Register" />
+                </form>
+                {
+                    registererror && <p className='text-red-500' >{registererror}</p>
+                }
+
+                {
+                    success && <p className='text-green-700' >{success}</p>
+                }
+
+                <p>Already Have an account <Link to='/login' >Please Login</Link></p>
             </div>
+
+
 
 
 
