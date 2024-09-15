23997221a1284d23ce42bf944d7e9f90b9525aa5
Shoeb1989 <shoebsikder1@gmail.com>
2024-09-15 23:19:32 +0600

    regex expression

--- user-email-password-auth/src/COMPO/Register/Register.jsx
+++ user-email-password-auth/src/COMPO/Register/Register.jsx
@@ -1,6 +1,14 @@
-import React from 'react';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import React, { useState } from 'react';
+import auth from '../../Firebase/Firebase.config';
 
 const Register = () => {
+      // error shows
+    const [registererror, setRegistererror] = useState('');
+    
+    // succ message
+
+    const [success, setSuccess] = useState('')
 
     const handleRegister = e => {
         e.preventDefault();
@@ -9,6 +17,34 @@ const Register = () => {
         const password = e.target.password.value;
 
         console.log(email, password);
+
+        if (password.length < 6) {
+            setRegistererror('Need 6 characters');
+            return;
+
+        }
+
+        // pass type 6 length
+
+
+        // reset error and success
+
+        setRegistererror('');
+        setSuccess('');
+
+        // create user
+
+     createUserWithEmailAndPassword(auth, email, password)
+     .then(result =>{
+        console.log(result.user);
+        setSuccess('Create Successfull')
+        
+     })
+     .catch(error => {
+        console.error(error);
+        setRegistererror(error.message);
+     })
+
     }
 
 
@@ -17,7 +53,7 @@ const Register = () => {
             <div className='mx-auto md:w-1/2 '  >
             <h2 className="text-3lx mb-8 ">Please Register</h2>
             <form onSubmit={handleRegister} >
-                <input className='mb-4 w-3/4 py-2 px-4 border-2 ' type="email" name="email" placeholder='Email Address'  />
+                <input className='mb-4 w-3/4 py-2 px-4 border-2 ' type="email" name="email" placeholder='Email Address' required  />
                 <br />
                 <br />
                 <input className='mb-4 w-3/4 py-2 px-4 border-2 ' type="password" name="password" placeholder='Password'  />
@@ -25,7 +61,16 @@ const Register = () => {
                 <br />
                 <input className='mb-4 w-3/4 btn btn-secondary ' type="submit" value="Register" />
             </form>
+            {
+                Error && <p className='text-red-500' >{registererror}</p>
+            }
+
+            {
+                success && <p className='text-green-700' >{success}</p>
+            }
             </div>
+
+
 
         </div>
     );
