2f6ff3ff92ca65eb75d41c1b95a7e9ba72ab8bb6
Shoeb1989 <shoebsikder1@gmail.com>
2024-09-30 20:59:35 +0600

    Phone API shows

--- Node js/my-phone-server/index.js
+++ Node js/my-phone-server/index.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const phones = require('./phones.json');
 const app = express();
 const port = 5000;
 
 app.get('/', (req, res) => {
 
-    res.send('my phone ola info commming soon')
+    res.send('my phone ola ola olao a info commming soon')
 
+})
+
+
+app.get ('/phones', (req, res) => {
+    res.send(phones);
 })
 
 app.listen(port,() => {
