b09528f862d247806f7859029470a30f36254e41
Shoeb1989 <shoebsikder1@gmail.com>
2024-10-08 00:08:57 +0600

    middleware install

---
+++ MongoDB/simple-curd-server/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+ 
+// middleware 
+app.use(cors());
+app.use(express.json); // for post value
+
+
+app.length('/', (req, res) => {
+    res.send('Simple crud is running')
+})
+
+app.listen(port, () => {
+    console.log(`Simple curd on port, ${port} `)
+})
