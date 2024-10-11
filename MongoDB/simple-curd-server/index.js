c4031dbaf31a7e6be495dd30c26a0c697ea9f4a3
Shoeb1989 <shoebsikder1@gmail.com>
2024-10-11 22:01:34 +0600

    sample for tasting

--- MongoDB/simple-curd-server/index.js
+++ MongoDB/simple-curd-server/index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 5000;
  
 // middleware 
 app.use(cors());
-app.use(express.json); // for post value
+app.use(express.json()); // for post value
 
 
 app.get('/', (req, res) => {
